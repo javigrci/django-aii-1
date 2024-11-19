13646414995281294d30e482b32b72edcff5b746
Julia Sanchez <julia.s.marquez@gmail.com>
2024-11-19 13:35:06 +0100

    modelos y urls sincronizadas

--- ej1/principal/models.py
+++ ej1/principal/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
 # Create your models here.
+
+class Partido(models.Model):
+    id = models.AutoField(primary_key=True)
+    equipo_local = models.CharField(max_length=50)
+    equipo_visitante=  models.CharField(max_length=50)
+    goles_local = models.PositiveIntegerField()
+    goles_visitante = models.PositiveIntegerField()
+    jornada = models.PositiveIntegerField()
+
+
+class Equipos(models.Model):
+    id = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=50)
+    año_fundacion = models.PositiveIntegerField
+    estadio = models.CharField(max_length=50)
+    aforo = models.PositiveIntegerField
+    direccion = models.CharField(max_length=150)
+
+
+class Jornada(models.Model):
+    numero = models.PositiveIntegerField
+    fecha = models.DateTimeField
+    temporada = models.CharField(max_length=10)
+
+class Temporada(models.Model):
+    año = models.PositiveIntegerField
